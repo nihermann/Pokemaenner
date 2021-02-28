9bc5e11332abc2e0cf43a59f925208442249f6d1
Nicolai <nihermann@uos.de>
2021-02-28 18:45:17 +0100

    added basic GAN classes

---
+++ GAN.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+class Discriminator(tf.keras.Model):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.layer_list = []
+
+    def call(self, x, training=False):
+        for layer in self.layer_list:
+            x = layer(x, training=training)
+        return x
+
+
+class Generator(tf.keras.Model):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.layer_list = []
+
+    def call(self, x, training=False):
+        for layer in self.layer_list:
+            x = layer(x, training=training)
+        return x
