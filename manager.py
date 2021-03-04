ba17153367f5a62f6741e85e5ca21fb21deb7218
Nicolai <nihermann@uos.de>
2021-03-05 00:20:05 +0100

    Added dummy self.data which still needs to be replaced. In forward_step: renamed partition -> trainings_frequency, solved data amount problem. In forward_step_discriminator: solved proportion real problem. In train: Partial implemented, included Manual abortion possibility. Renamed generate_pictures -> generate_images Added generate_and_save_images function

--- manager.py
+++ manager.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import GAN
+import utils
 
 
 class GANManager:
@@ -22,6 +23,8 @@ class GANManager:
         # make loss and optimizers as model params?
         self.loss_function = kwargs["loss"]
         self.optimizer = kwargs["optimizer"]
+
+        self.data = tf.ones((128, 32, 32, 3))  # todo add data
 
     def get_noise(self, batch_size=None, mu=0.5, sd=0.5):
         """
@@ -68,7 +71,7 @@ class GANManager:
         )
         return loss_for_real_images + loss_for_generated_images
 
-    def forward_step(self, real_images, partition, proportion_real=0.5):
+    def forward_step(self, real_images, trainings_frequency, proportion_real=0.5):
         """
         Compute one forward step
         :param real_images: Input one batch of real images to train the Discriminator
@@ -76,10 +79,10 @@ class GANManager:
         :param  proportion_real: float[0:1] - how much percent of the should be replaced by generated images?
         :return: float - Generator loss, float - Discriminator loss
         """
-        generator_losses = [self.forward_step_generator() for _ in range(partition[0])]
-        # TODO solve real_images problem as they stay the same for n_d iterations
-        discriminator_losses = [self.forward_step_discriminator(real_images, proportion_real=proportion_real) for _ in
-                                range(partition[1])]
+        generator_losses = [self.forward_step_generator() for _ in range(trainings_frequency[0])]
+        # gets automatically passed trainings_frequency[1] many partial batches.
+        discriminator_losses = [self.forward_step_discriminator(images, proportion_real=proportion_real) for images in
+                                real_images]
 
         return tf.reduce_mean(generator_losses), tf.reduce_mean(discriminator_losses)
 
@@ -90,7 +93,7 @@ class GANManager:
         """
         with tf.GradientTape() as tape:
             # Generate images from noise
-            generated_images = self.generate_pictures(training=True)
+            generated_images = self.generate_images(training=True)
 
             # try to fool the discriminator and assess our success.
             prediction = self.discriminator(generated_images, training=False)
@@ -109,22 +112,17 @@ class GANManager:
         :param proportion_real: float[0:1] - how much percent of the batch should be replaced by generated images?
         :return: float - loss for the discriminator.
         """
-        assert 0 <= proportion_real <= 1, "Proportion must be between 0-1"
 
         with tf.GradientTape() as tape:
             # generate some images to train the discriminator corresponding to the inverse proportion.
-            generated_images = self.generate_pictures(int((1 - proportion_real) * self.batch_size))
+            generated_images = self.generate_images(int((1 - proportion_real) * self.batch_size))
 
             # take the remaining portion from our real images.
-            take_reals_until = tf.cast(tf.math.ceil(self.batch_size * proportion_real), tf.int32)
-            train_batch = tf.concat((real_images[:take_reals_until], generated_images), axis=0)
+            train_batch = tf.concat((real_images, generated_images), axis=0)
 
             # train the discriminator and assess its performance.
             predictions = self.discriminator(train_batch, training=True)
-            loss = self.discriminator_loss(
-                real_images_prediction=predictions[:take_reals_until],
-                generated_images_prediction=predictions[:take_reals_until]
-            )
+            loss = self.discriminator_loss(real_images_prediction=predictions, generated_images_prediction=predictions)
 
             # improve discriminator based on its assessed loss.
             gradients = tape.gradient(loss, self.discriminator.trainable_variables)
@@ -132,17 +130,46 @@ class GANManager:
 
         return loss
 
-    def train(self, epochs, print_every=5, save_each=5):
-        """ # TODO write docstring
-
-        :param epochs:
-        :param print_every:
-        :param save_each:
-        :return:
-        """
+    def train(
+            self,
+            epochs,
+            samples_per_epoch=1200,
+            trainings_frequency=(5, 5),
+            proportion_real=0.5,
+            print_every=5,
+            save_pictures_every=5,
+            how_many_pictures_to_save=20,
+            save_model_every=5
+    ):
+        # TODO write docstring
         # TODO eventually sample or save some generated pictures. Maybe sampled ones with download link?
-        # TODO implement train function.
-        pass
+
+        assert 0 <= proportion_real <= 1, "Proportion must be between 0-1"
+
+        # how much real images we want to take to get the right proportion given the batch size
+        take_reals = tf.cast(tf.math.ceil(self.batch_size * proportion_real) * trainings_frequency[1], tf.int32)
+
+        generator_losses_accumulator, discriminator_losses_accumulator = [], []
+        try:  # Like this we will have to opportunity to end training early and save the model.
+            for epoch in range(epochs):
+                count = 0
+                while count < samples_per_epoch:
+                    count += samples_per_epoch
+
+                    data = self.data.take(take_reals)
+                    generator_loss, discriminator_loss = self.forward_step(data, trainings_frequency, proportion_real)
+
+                    generator_losses_accumulator.append(generator_loss)
+                    discriminator_losses_accumulator.append(discriminator_loss)
+        # ----------- Early Abortion ------------
+        except KeyboardInterrupt:
+            if "1" == input("Manual abortion.. to save the current model answer '1'"):
+                try:
+                    self.save_model()
+                except Exception as e:
+                    print("Saving the Model was unsuccessful with the exception", type(e), e)
+        # ---------------------------------------
+
 
     def save_model(self):
         # TODO implement save_model function - maybe only save weights?!
@@ -159,17 +186,28 @@ class GANManager:
         # TODO load only one model?!
         pass
 
-    def generate_pictures(self, number=None, save_to="", training=False):
+    def generate_images(self, number=None, training=False):
         """
         Returns pictures using the Generator.
         :param number: int - number of pictures.
-        :param save_to: string - if specified, pictures will be saved to the given path.
         :param training: bool - is the generator in training?
         :return: The above specified number of generated pictures.
         """
         noise = self.get_noise(number)
         images = self.generator(noise, training=training)
-        if save_to:
-            # TODO implement saving
-            pass
         return images
+
+    def generate_and_save_images(self, number, save_to="", prefix="", return_images=False):
+        """
+        Generates images with the current managers generator.
+        :param number: int - number of images to create.
+        :param save_to: string - path to the destination directory. Don't forget the '/' at the end!
+        :param prefix: string - name prefix for saved images.
+        :param return_images: bool - weather the images should also be returned or not.
+        :return: if return_images==True the generated pictures will be returned.
+        """
+        images = self.generate_images(number, training=False)
+        utils.save_images(images, save_to, prefix)
+
+        if return_images:
+            return images
