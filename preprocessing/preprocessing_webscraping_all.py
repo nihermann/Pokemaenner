c5efb4900eaf47955abb3798e073d05febd49ec3
Nicolai <nihermann@uos.de>
2021-03-28 01:29:55 +0100

    added remove corrupted images function and reformatted file

--- preprocessing/preprocessing_webscraping_all.py
+++ preprocessing/preprocessing_webscraping_all.py
@@ -1,26 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-import cv2
 from preprocessing_uniform_data import uniform, resize, center_focus, resize_and_save
+import io
+import PIL
 from PIL import Image
-import numpy as np
-import sys
+
 
 # gets all the pokemon sites of their list
-def get_pokemon(url, identifier = "pokedex"):
+def get_pokemon(url, identifier="pokedex"):
     soup = get_soup(url)
     # find all the attributed tags (links that contain more information aka lead to
-    # a differnet site here: the pokemon) from the website
+    # a different site here: the pokemon) from the website
     pokemon_urls = []
     for a in soup.find_all('a', href=True):
         # of all the attributed tags get only the ones that are pokemon (identifiable by the url (pokedex/sprites))
-        if identifier in a['href'] :
+        if identifier in a['href']:
             # append this tag with the base link to form the complete link to the pokemon
-            pokemon_urls.append('https://pokemondb.net'+ a['href'])
+            pokemon_urls.append('https://pokemondb.net' + a['href'])
     all_pokemon = list(set(pokemon_urls))
     all_pokemon.sort()
     return all_pokemon
+
 
 def listed_sprites(soup):
     # find all images in the given parsed html file
@@ -33,10 +34,12 @@ def listed_sprites(soup):
 
     return listed_pokemon
 
+
 def listed_artwork(soup):
     originals = [image['src'] for image in soup.find_all('img')]
     alternatives = [image['href'] for image in soup.find_all('a', href=True) if 'artwork' in image['href']]
-    return originals+alternatives
+    return originals + alternatives
+
 
 def get_soup(url):
     # request the given url
@@ -45,23 +48,24 @@ def get_soup(url):
     soup = BeautifulSoup(r.text, 'html.parser')
     return soup
 
+
 def download(images):
     # for every image in the list make the filename valid and
     # and download it
     for image in images:
-        name = image.replace('https://img.pokemondb.net/','').replace(' ', '_').replace('-', '_').replace('/', '_').replace('&', '_')
+        name = image.replace('https://img.pokemondb.net/', '').replace(' ', '_').replace('-', '_').replace('/', '_').replace('&', '_')
 
         if '_back_' not in name and '.gif' not in name:
             with open(name, 'wb') as f:
                 im = requests.get(image)
                 f.write(im.content)
-            uniform(os.path.join(os.getcwd(),name), 128, 128)
+            uniform(os.path.join(os.getcwd(), name), 128, 128)
 
 
-def imagedown(url,folder):
+def imagedown(url, folder):
     try:
-        folder = os.path.join(os.getcwd(),folder)
-        # making a direcotry by joining the current one with the given one
+        folder = os.path.join(os.getcwd(), folder)
+        # making a directory by joining the current one with the given one
         os.mkdir(folder)
     except:
         pass
@@ -83,6 +87,17 @@ def imagedown(url,folder):
     path_parent = os.path.dirname(os.getcwd())
     os.chdir(path_parent)
 
+
+def remove_corrupted_images(path):
+    for file in os.listdir(path):
+        try:
+            with open(os.path.join(path, file), 'rb') as f:
+                img = Image.open(io.BytesIO(f.read()))
+        except PIL.UnidentifiedImageError as e:
+            os.remove(os.path.join(path, file))
+            print(f"Removed {file} because it is corrupted.")
+
+
 if __name__ == "__main__":
     # for every pokemon found in the first site (the national pokedex) find every possible sprite for it
     # and make a new folder with its name (or not)
@@ -91,8 +106,10 @@ if __name__ == "__main__":
     for pokemon in get_pokemon(url)[1:]:
         try:
             count = count + 1
-            imagedown(pokemon, folder = "data")
-            print(f"Number of downloaded pokemon: {count}/903. Current pokemon: {pokemon.replace('https://pokemondb.net/pokedex/','')}",sep='',end="\r",flush=True)
+            imagedown(pokemon, folder="data")
+            print(
+                f"Number of downloaded pokemon: {count}/903. Current pokemon: {pokemon.replace('https://pokemondb.net/pokedex/', '')}",
+                sep='', end="\r", flush=True)
             print()
         except:
             # move to the original path
@@ -105,8 +122,7 @@ if __name__ == "__main__":
     # if you can see that there are wrongly sized images in your data folder (checks before resizing but still)
     for image in images:
         if "artwork" in image:
-            resize_and_save(os.path.join(os.getcwd(),"data",image),128,128)
-
+            resize_and_save(os.path.join(os.getcwd(), "data", image), 128, 128)
 
     # for image in images:
     #     image_path = os.path.join(os.getcwd(),"data", image)
