8dea224080e572616fa7b01d5dfba3ba77cde04d
Nicolai <nihermann@uos.de>
2021-03-30 13:15:46 +0200

    unrelevant

--- test.py
+++ test.py
@@ -68,14 +68,54 @@ def linear(x, y):
     return y
 
 
+class A:
+    def __init__(self, a):
+        self.a = a
+
+    def add(self, x, y):
+        return self(x, y)
+
+    def __call__(self, a, b):
+        return a - b + self.a
+
+class B:
+    def __init__(self, b=2):
+        self.b = b
+    def subtract(self, x, y):
+        return x - y
+
+    def __call__(self, a, b):
+        return a + b + self.b
+
+def add_attr(a, b, name):
+    attr = getattr(a, name)
+    
+    def f(x, y):
+        return f.func(b, x, y)
+    
+    f.__setattr__("func", attr)
+    b.__setattr__(name, f)
+
+def add_attr2(a, b, name):
+    import types
+    attr = getattr(a, name)
+    setattr(b, name, types.MethodType(attr, b))
+
+
 if __name__ == '__main__':
-    mlp = MLP()
+    # mlp = MLP()
+    #
+    # x = np.random.random((1000, 16))
+    # y = np.ones((1000, 2))
+    # y[:, 1] = 0
+    #
+    # mlp.fit(x, y, epochs=20)
+    from utils import transfer_method
+    a = A(10)
+    b = B()
+    # attr = getattr(A, "add")
+    # b = transfer_method("add", A, b)
+    # b.__setattr__("add", attr)
+    add_attr2(A, b, "add")
+    print()
 
-    x = np.random.random((1000, 16))
-    y = np.ones((1000, 2))
-    y[:, 1] = 0
-
-    mlp.fit(x, y, epochs=20)
-
-    # m = Manager(mlp)
-    # m.forward_step(x, y, epochs=3)
